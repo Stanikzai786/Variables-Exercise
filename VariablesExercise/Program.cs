7e5187bdc6ea934b6d6be77013acf685c24e7f9b
Ahmad Stanikzai <Shekibstanikzai.2016@gmail.com>
2022-11-21 08:25:58 -0800

    Made changes

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,19 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            Console.WriteLine("Hello, C32!");
+            Console.WriteLine("please enter your name:  ");
+            string name = Console.ReadLine();
+            Console.WriteLine("please enter your cat color:  ");
+            string catColor = Console.ReadLine();
+            Console.WriteLine("enter your CatAge:  ");
+            int age = int.Parse(Console.ReadLine());
+
+
+            //string interoplation
+            Console.WriteLine($"Hello, {name}. Your cats color is, {catColor}.");
+            Console.WriteLine($"And Your cat's Age is {age}");
         }
     }
 }
